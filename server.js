772a629e7334b268926030ef3004ffb246df9891
Jerrorld <jerrorldk@gmail.com>
2022-09-15 18:35:19 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 // LISTEN
 app.listen(PORT, () => {
@@ -25,3 +29,4 @@ app.listen(PORT, () => {
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
