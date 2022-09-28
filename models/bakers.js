4419f654f527344926955673b9b9af0c4b8de433
Jerrorld <jerrorldk@gmail.com>
2022-09-27 21:18:35 -0700

    added Bakers into website

--- models/bakers.js
+++ models/bakers.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const Bread = require('./bread')
 
-
+// Schema
 const bakerSchema = new Schema({
     name: {
         type: String,
@@ -12,9 +13,15 @@ const bakerSchema = new Schema({
         type: Date,
         required: true
     },
-    bio: String
-})
+    bio: String,
+}, { toJSON: { virtuals: true }})
 
+// Virtuals
+bakerSchema.virtual('breads', {
+    ref: 'Bread',
+    localField: '_id',
+    foreignField: 'baker'
+})
 
 const Baker = mongoose.model('Baker', bakerSchema)
 module.exports = Baker
